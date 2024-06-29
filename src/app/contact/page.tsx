7ec8a2f13cf226e1da8ecda55a6a7c35fbcaf793
Aman Panwar <amanpanwar1672@gmail.com>
2024-06-29 16:18:34 +0530

    second commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Meteors } from "@/components/ui/meteors";
+function Page() {
+  return (
+    <div className="w-screen h-screen">
+    <Meteors number={100} />
+  </div>
+
+  );
+}
+
+export default Page;
